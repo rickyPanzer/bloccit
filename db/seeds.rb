a199d05ae19dfbb4b2fc8feb07f2ccd6e6bee645
rickyPanzer <richardrpanzer@gmail.com>
2015-01-20 13:39:48 -0500

    associating-with-users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,22 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end
+users = User.all
+
 50.times do 
   Post.create!(
-    title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph
+    user:   users.sample,
+    title:  Faker::Lorem.sentence,
+    body:   Faker::Lorem.paragraph
   )
 end
 posts = Post.all
@@ -17,10 +29,18 @@ posts = Post.all
 #Create Comments
 100.times do
   Comment.create!(
+    # user: users.sampe,
     post: posts.sample,
     body: Faker::Lorem.paragraph
   )
 end
+
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: 'rrp54@cornell.edu',
+  password: 'helloworld'
+)
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
